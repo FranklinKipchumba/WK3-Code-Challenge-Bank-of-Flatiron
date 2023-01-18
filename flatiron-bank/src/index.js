199748780788acc52b513687dc9e4be410485afd
Franklin Kipchumba <sitieneifranklin@gmail.com>
2023-01-18 10:10:09 +0300

    Completed assignment

--- flatiron-bank/src/index.js
+++ flatiron-bank/src/index.js
@@ -1,35 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import{
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
 
-import { Dashboard } from './dashboard/dashboard'
-import { NewCustomer } from './new-customer/new-customer'
-import { Deposit } from './deposit/deposit'
-import { Transfer } from './transfer/transfer'
-import { Withdraw } from './withdraw/withdraw'
-import { Balance } from './balance/balance'
-import { Transaction } from './Transaction Table/transaction'
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Dashboard />}/>
-      <Route path='/table' element={<Transaction />}/>
-      <Route path='/new' element={<NewCustomer />}/>
-      <Route path='/deposit' element={<Deposit />}/>
-      <Route path='/withdraw' element={<Withdraw />}/>
-      <Route path='/transfer' element={<Transfer />}/>
-      <Route path='/balance' element={<Balance />}/>
-    </Routes>
-  </BrowserRouter>
-);
-
-
+ReactDOM.render(<App />, document.getElementById("root"));
